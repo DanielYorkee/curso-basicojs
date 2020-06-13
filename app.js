74db8b91f6f8338ed4bcc7a545779b9960604f69
Daniel Ramirez <danielramirezyk@gmail.com>
2020-06-13 01:02:15 -0400

    curso javascript inicio sin comentarios

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ console.log({"username": "ryan", "score": "124.32", "campeon": "true"})
 var nombreUsuario = "Daniel";
 
 //reasigando valor variable daniel
-var nombreUsuario = "Andrea";// el codigo se ejecuta de arriba hacia abajo por lo que se reemplaza
-                            //daniel por andrea, por eso se llaman variables porque pueden variar
-let lastName = "Ramirez"; // let es otra forma de declarar variables modificables
+var nombreUsuario = "Andrea";
+
+let lastName = "Ramirez";
 
 console.log(nombreUsuario,lastName);
 
-const numeroPiCalculadora = "3.1415"; // una constante que no cambiara a lo largo del codigo
-                //si se intentara cambiar como con la var arrojarìa error en console
+const numeroPiCalculadora = "3.1415";
+
 console.log(numeroPiCalculadora);
 var $casa = "techada";
 var _guitarra = "acustica";
 console.log($casa);
-console.log(_guitarra); // estos son los caracteres especiales que acepta, no se pueden empezar con numeros
+console.log(_guitarra);
 
 //camelcase = forma de escribir palabras para que sean faciles de leer
 let userName = "yorke";
@@ -34,18 +34,18 @@ console.log(resultado);
 //comparando
 
 let mayorQue = numberOne > numberTwo;
-console.log(mayorQue); // darà un true o false dependiendo si se cumple la condicion
+console.log(mayorQue);
 let esIgual = numberOne == numberTwo;
-console.log(esIgual); // darà un true o false dependiendo si se cumple la condicion
-let distintoQue = numberOne != numberTwo; // diferente uno del otro
-console.log(distintoQue); // darà un true o false dependiendo si se cumple la condicion
+console.log(esIgual);
+let distintoQue = numberOne != numberTwo;
+console.log(distintoQue);
 
 
 
 let name = "miguel";
 let apellido = "contreras";
 
-let nombreCompleto = (name + ' ' + apellido); // concatenacion  
+let nombreCompleto = (name + ' ' + apellido); // concatenacion
 
 console.log(nombreCompleto);
 
@@ -57,19 +57,17 @@ let input = "pepasdas23"
 
 let resultadoPass = input == passwordDB;
 
-console.log(resultadoPass); //demuestra que no hay alcance en los datos ingresados por 
-//el cliente y la supuesta contraseña alamacenada en la db
-
+console.log(resultadoPass);
 
 //control de flujo
 //condicionales
 
 if (resultadoPass == true) {
     console.log("Login correcto");
-    
+
 } else {
     console.log("login incorrecto");
-    
+
 }
 
 let score = 31;
@@ -79,14 +77,14 @@ if (score > 30) {
 else if (score > 15) {
     console.log("estás mejorando");
 
-}    
-    
- else {
-    console.log("necesitas seguir el tutorial");
-    
 }
 
-// switch case 
+ else {
+    console.log("necesitas seguir el tutorial");
+
+}
+
+// switch case
 let TypeCard = "Debid Card";
 switch (TypeCard) {
     case 'Debid Card':
@@ -96,40 +94,40 @@ switch (TypeCard) {
     case 'Credit Card':
         console.log("this is a credit card");
         break;
-    
+
     default:
-    console.log("no card");   
+    console.log("no card");
 }
 
 //iterador o bucles
 //ciclo while
 
-let count = 50; //primero se asigna una variable contador y la utilizaremos como condicion
-while (count > 0) { // la condicion se cumple pero aun el bucle no esta creado
+let count = 50;
+while (count > 0) {
     console.log("hello world");
-    count = count - 1;  // esto lo crea, un vez que muestra el hello world, el count disminuye en uno, es 49 y vuelve a repetirse el while
-    
+    count = count - 1;
+
 }
 
-let contador = 3; //primero se asigna una variable contador y la utilizaremos como condicion
-while (contador > 0) { // la condicion se cumple pero aun el bucle no esta creado
-    console.log(contador); // ahora mostrará la variable contador 
-    contador = contador - 1;  // esto lo crea, un vez que muestra el numero dismuye el 20 en 19 y se repite el ciclo
-    
+let contador = 3;
+while (contador > 0) {
+    console.log(contador);
+    contador = contador - 1;
+
 }
 //iniciando el contador en 0 y aumentando en 1
 console.log("esto es para separar");
-let conta = 0; 
-while (conta < 6) { 
-    console.log(conta); 
-    conta++;  
+let conta = 0;
+while (conta < 6) {
+    console.log(conta);
+    conta++;
 }
 console.log("esto es para separar");
 
-let contad = 10; 
-while (contad >  0) { 
-    console.log(contad); 
-    contad--;  
+let contad = 10;
+while (contad >  0) {
+    console.log(contad);
+    contad--;
 }
 
 console.log("esto es para separar");
@@ -138,7 +136,7 @@ console.log("esto es para separar");
 let nombres = ['michael', 'jonas', 'ulrich'];
 console.log(nombres[1]);
 console.log(nombres.length);
- 
+
 for (let i = 0; i < nombres.length; i++) {
     console.log(nombres[i]);
 }
@@ -149,11 +147,11 @@ for (let i = nombres.length - 1; i >= 0; i--){
 //funciones o metodos
 //definir comportamiento y reutilizarlo
 
-function saludar(params) { //creando el metodo
+function saludar(params) {
 
     console.log('hello');
 }
-saludar(); //llamando el metodo, sin esto no aparece nada
+saludar();
 
 
 function sumar (n1, n2){
@@ -161,5 +159,3 @@ function sumar (n1, n2){
 }
 sumar(100, 200);
 sumar(2342, 3453);
-
-
